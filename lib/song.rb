ee2011694415ad41a298195ac328b81388059b33
rebeccaeli20 <rebeccaeli20@gmail.com>
2018-07-13 22:38:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,21 @@
+class Song
+  attr_reader :name, :artists, :genre
+  attr_writer :name, :artists, :genre
+  @@all_songs =[]
+  def initialize(name, artists, genre)
+    @name = name
+    @artists = artists
+    @genre = genre
+    @@all_songs << self
+  end
+  def self.all_songs
+    @@all_songs 
+  end 
+  
+  
+end 
 
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+puts ninety_nine_problems.name
+all_songs_array = Song.all_songs 
+puts all_songs_array.length
